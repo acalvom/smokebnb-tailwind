507dc93e30afbf0221f82aa7b389000d697452c6
acalvom <andreac.3108@gmail.com>
2023-09-11 10:25:41 +0200

    feat: customize theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,28 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#eb5758',
-        'black': '#000000',
-        'gray': '#edf2f4',
+        primary: '#EB5757',
+        primaryText: '#333',
+        secondaryText: '#828282',
+        lightText: '#BDBDBD',
+        gray: '#4F4F4F',
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        mulish: ['Mulish', 'sans-serif'],
+      },
+      fontSize: {
+        xl: '1.5rem',
+        lg: '1rem',
+        md: '0.875rem',
+        sm: '0.75rem',
+        xs: '0.5rem',
+      },
+      boxShadow: {
+        all: '0px 1px 6px 0px rgba(0, 0, 0, 0.10)',
       },
     },
   },
   plugins: [],
-}
+};
